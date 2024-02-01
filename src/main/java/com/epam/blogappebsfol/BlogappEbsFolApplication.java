05112ea1c625f425a9975af020cbb9c7f6374fc1
Viacheslav_Poliakov <polyakov_slava@ukr.net>
2024-02-01 18:37:43 +0200

    Initial commit

---
+++ src/main/java/com/epam/blogappebsfol/BlogappEbsFolApplication.java
@@ -0,0 +1,13 @@
+package com.epam.blogappebsfol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogappEbsFolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogappEbsFolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/blogappebsfol/BlogappEbsFolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.blogappebsfol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappEbsFolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
