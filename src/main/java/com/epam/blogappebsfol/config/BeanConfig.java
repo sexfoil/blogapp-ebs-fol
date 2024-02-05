c0bad6a73dfed11b1dcabfffbc9481795720bcb7
Viacheslav_Poliakov <polyakov_slava@ukr.net>
2024-02-05 09:58:38 +0200

    Added postman collection

--- src/main/java/com/epam/blogappebsfol/config/BeanConfig.java
+++
@@ -1,7 +0,0 @@
-package com.epam.blogappebsfol.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class BeanConfig {
-}
--- src/main/java/com/epam/blogappebsfol/controller/PostController.java
+++ src/main/java/com/epam/blogappebsfol/controller/PostController.java
@@ -1,11 +1,8 @@
 package com.epam.blogappebsfol.controller;
 
 import com.epam.blogappebsfol.domain.dto.PostDto;
-import com.epam.blogappebsfol.domain.entity.PostEntity;
-import com.epam.blogappebsfol.domain.entity.TagEntity;
 import com.epam.blogappebsfol.service.PostService;
 import com.epam.blogappebsfol.service.TagService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
